aea9a6926f597a93bf6e1868586a388cb349314d
Alexander <ajspottswood@gmail.com>
2019-06-04 17:57:05 -0400

    added delete profile & user route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -129,4 +129,21 @@ router.get('/user/:user_id', async (req, res) => {
   }
 });
 
+// @route  DELETE api/profile
+// @desc   Delete profile
+// @access Public
+
+router.delete('/', auth, async (req, res) => {
+  try {
+    //Remove profile
+    await Profile.findOneAndRemove({ user: req.user.id });
+    //Remove User
+    await User.findOneAndRemove({ _id: req.user.id})
+    res.json({ msg: 'User has been removed' });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
 module.exports = router;
