7e21369e3f620817893efe231dfddb9d2c430466
Alexander <ajspottswood@gmail.com>
2019-06-06 13:35:46 -0400

    added get profile github repo route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const request = require('request');
+const config = require('config');
 const router = express.Router();
 const auth = require('../../middleware/auth');
 const { check, validationResult } = require('express-validator/check');
@@ -190,7 +192,7 @@ try {
   res.json(profile);
 } catch (err) {
   console.error(err.message);
-  res.status(500).send({ msg: 'Profile not found'});
+  res.status(500).send('Server Error');
 }
 });
 
@@ -211,7 +213,7 @@ router.delete('/experience/:exp_id', auth, async (req, res) => {
     res.json(profile);
   } catch (err) {
     console.error(err.message);
-    res.status(500).send({ msg: 'Profile not found'});
+    res.status(500).send('Server Error');
   }
 });
 
@@ -260,7 +262,7 @@ try {
   res.json(profile);
 } catch (err) {
   console.error(err.message);
-  res.status(500).send({ msg: 'Profile not found'});
+  res.status(500).send('Server Error');
 }
 });
 
@@ -281,8 +283,35 @@ router.delete('/education/:edu_id', auth, async (req, res) => {
     res.json(profile);
   } catch (err) {
     console.error(err.message);
-    res.status(500).send({ msg: 'Profile not found'});
+    res.status(500).send('Server Error');
   }
 });
+
+// @route  GET api/profile/github/:username
+// @desc   Get a user repo github
+// @access Public
+
+router.get('/github/:username', async (req, res) => {
+  try {
+    const options = {
+      uri: `https://api.github.com/users/${req.params.username}/repos?per_page=5&sort=created:asc&client_id=${config.get('githubClientId')}&client_secret=${config.get('githubSecret')}`,
+      method: 'GET',
+      headers: { 'user-agent': 'node.js'}
+    };
+
+    request(options, (error, response, body) => {
+      if(error) console.error(error);
+
+      if(response.statusCode !== 200) {
+        return res.status(404).json({ msg: 'No Github profile found'});
+      }
+
+      res.json(JSON.parse(body));
+    })
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+})
 
 module.exports = router;
