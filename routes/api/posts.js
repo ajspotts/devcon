53d87295a00699dd95eaaefdd5ce8ba268df6e8a
Alexander <ajspottswood@gmail.com>
2019-06-07 17:51:44 -0400

    added get post by id route

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -51,6 +51,24 @@ router.get('/', auth, async (req, res) => {
     console.error(err);
     res.status(500).send('Server Error')
   }
+});
+
+// @route  Get api/posts/:id
+// @desc   Get post by ID
+// @access Private
+
+router.get('/:id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    if(!post) return res.status(404).json({ msg: 'Post not found' });
+    
+    res.json(post);
+  } catch (err) {
+    if(err.kind == 'ObjectId') return res.status(404).json({ msg: 'Profile not found' });
+    console.error(err.message);
+    res.status(500).send('Server Error')
+  }
 })
 
 module.exports = router;
