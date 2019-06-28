736db933be55f73f69c5877c6f23377c410fd061
Alexander <ajspottswood@gmail.com>
2019-06-28 17:28:58 -0400

    added GET_REPOS to reducer

--- client/src/reducers/profile.js
+++ client/src/reducers/profile.js
@@ -1,6 +1,7 @@
 import {
   GET_PROFILE,
   GET_PROFILES,
+  GET_REPOS,
   PROFILE_ERROR,
   CLEAR_PROFILE,
   UPDATE_PROFILE
@@ -27,6 +28,12 @@ export default function(state = initialState, { type, payload }) {
       return {
         ...state,
         profiles: payload,
+        loading: false
+      };
+    case GET_REPOS:
+      return {
+        ...state,
+        repos: payload,
         loading: false
       };
     case PROFILE_ERROR:
