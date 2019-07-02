ad8d91ec857704d059369dad0460b4aaf3338eb2
Alexander <ajspottswood@gmail.com>
2019-07-02 19:49:48 -0400

    created post reducer and action types

---
+++ client/src/actions/post.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+import { setAlert } from './alert';
+import { GET_POSTS, POSTS_ERROR } from './types';
+
+export const getPosts = () => async dispatch => {
+  try {
+    const res = axios.get('/api/posts');
+
+    dispatch({
+      type: GET_POSTS,
+      payload: res.data
+    });
+  } catch (err) {
+    dispatch({
+      type: POSTS_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status }
+    });
+  }
+};
